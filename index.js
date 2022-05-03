d9712481f665da18e538dbe71ce2c918a70b89ea
Beyhan <beyhan.taylan@hotmail.com>
2022-05-03 14:36:53 -0400

    commit tree b50387d58d68d2392cb44ddcf9630a705c1962aa

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const element = document.getElementById("main")
+element.remove()
+
+document.querySelector('h1')
+
+const newHeader = document.createElement('h1')
+newHeader.id= 'victory'
+
+newHeader.innerHTML = ("Beyhan is the champion")
+document.body.append(newHeader)
